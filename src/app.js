abca883cef195b8af10de64e5526fc3186b740c1
kalyan <kalyanlee3@gmail.com>
2025-03-31 18:26:42 +0530

    practice dynamic routes

--- src/app.js
+++ src/app.js
@@ -3,27 +3,9 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
+app.get("/user/:userId/:profileId", (req, res) => {
+  console.log(req.params);
   res.send({ firstName: "Henry", LastName: "John" });
-});
-
-app.post("/user", (req, res) => {
-  // save user details in DB
-  res.send("User details are successfully saved in database!");
-});
-
-app.patch("/user", (req, res) => {
-  // update user details in DB
-  res.send("User details are successfully updated in database!");
-});
-
-app.delete("/user", (req, res) => {
-  // delete user details in DB
-  res.send("User details are successfully deleted in database!");
-});
-
-app.use("/user", (req, res) => {
-  res.send("test from the server");
 });
 
 app.listen(PORT, () => {
