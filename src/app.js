c5f10ddef5b72e9aafc79b5453ae8b4fcfc8c4b7
kalyan <kalyanlee3@gmail.com>
2025-04-01 10:01:03 +0530

    play with multiple routes

--- src/app.js
+++ src/app.js
@@ -3,10 +3,33 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId/:profileId", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Henry", LastName: "John" });
-});
+app.use("/user", [
+  (req, res, next) => {
+    console.log("handle first route");
+    // res.send("response 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handle second route");
+    // res.send("response 2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handle third route");
+    // res.send("response 3");
+    next();
+  },
+
+  (req, res, next) => {
+    console.log("handle fourth route");
+    // res.send("response 4");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handle fifth route");
+    res.send("response 5");
+  },
+]);
 
 app.listen(PORT, () => {
   console.log(`server successfully listen at port ${PORT}`);
